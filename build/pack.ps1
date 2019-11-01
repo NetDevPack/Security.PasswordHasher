0605b779cf120bc1cd673622071f0f724acf53fa
Bruno Brito <bhdebrito@gmail.com>
2019-11-01 02:28:04 -0300

    config

---
+++ build/pack.ps1
@@ -0,0 +1,11 @@
+$version = $args[0]
+
+if (!$version)
+{
+    $version = '3.0.2-prerelease1.' + $([System.DateTime]::Now.ToString('MM-dd-HHmmss'))
+}
+Write-Host 'Version: ' $version 
+get-childitem * -include *.nupkg | remove-item
+dotnet build ..\src\JpProject.PasswordHasher.sln
+dotnet test ..\src\JpProject.PasswordHasher.sln
+dotnet pack ..\src\JpProject.PasswordHasher.sln -o .\ -p:PackageVersion=$version
